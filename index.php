7d35a11c7ebdd9c623842c9c7ee098a1c3525bc0
khushboo0012 <f20170012@pilani.bits-pilani.ac.in>
2019-07-09 08:18:50 +0530

    final commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<link rel = "stylesheet" type="text/css" href="middle.css">
+<html>
+<br>
+<br>
+<br>
+<body style = "color:white">
+<h3 align="center">Enter Pret And Post Time to proceed</h3>
+<div id = "form-wrapper">
+<form action=FetchData.php method="get" target="_blank">
+  Pre time: <input type="number", name="Pre" class="click" required/><br> <br>
+  Pos time: <input type="number", name="Pos" class="click" required/><br>
+  <button name="Submit" class="click">Submit</button>
+</form>
+</div>
+</br>
+</br>
+</br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</body>
+
+<?php include 'decorate.html' ?>
+</html>
